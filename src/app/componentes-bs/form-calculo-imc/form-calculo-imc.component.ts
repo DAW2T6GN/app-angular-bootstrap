d5c33d6247dbf42870e627e99c9676c2b2da19a1
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-06-10 19:55:24 -0500

    formulario de IMC

--- src/app/componentes-bs/form-calculo-imc/form-calculo-imc.component.ts
+++ src/app/componentes-bs/form-calculo-imc/form-calculo-imc.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class FormCalculoImcComponent {
 
+  frmImc = {
+    nombre: null,
+    peso: null,
+    talla: null,
+    imc: 0
+  }
+
+  resultado = ""
+
+  lstPacientes: Paciente = []
+
+  calcularIMC(){
+    let tallam = Number(this.frmImc.talla) / 100;
+    this.frmImc.imc = Number(tallam) / (Number(this.frmImc.peso) * Number(this.frmImc.peso))
+    this.resultado = "Su IMC es "+ this.frmImc.imc
+    this.lstPacientes.push({nombre: String(this.frmImc.nombre), imc: this.frmImc.imc})
+  }
 }
+type Paciente = Array<{nombre: string, imc: number }>;
